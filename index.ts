ad867c4760cd63b986185060834ae8e82d9dc159
Luigi Donadel <luigi.donadel@gmail.com>
2024-01-14 16:40:28 +0100

    getting tasks

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import axios from 'axios';
 
 type Task = {
+    id: String
     name: String
     project: Project
 }
@@ -11,24 +12,41 @@ type Project = {
 }
 
 const fullName = (task : Task): String => {
-    return `${task.project.name}/${task.name}`
+    return `${task.project.name}: ${task.name}`
 }
 
 const WORKSPACE_ID = process.env.WORKSPACE_ID ?? ""
 const CLOCKIFY_API_KEY = process.env.CLOCKIFY_API_KEY ?? ""
 
+console.log("Env", process.env)
 const clockify = axios.create({
     baseURL: `https://api.clockify.me/api/v1/workspaces/${WORKSPACE_ID}`,
     timeout: 1000,
     headers: {'x-api-key': CLOCKIFY_API_KEY}
   });
 const getTasks = async (project: Project): Promise<Task[]> => {
+    console.log("GetTasks", project.name)
     const { data } = await clockify.get(`/projects/${project.id}/tasks`)
     return data
 
 }
+const getAllTasks = async (projects: Project[]): Promise<Task[]> => {
+    var output: Task[] = []
+    for (const project of projects) {
+        await delay(50)
+        const tasks = (await getTasks(project)).map(v => {
+            return {...v, project: project}
+        })
+        output = output.concat(tasks)
+    }
+    return output
+}
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
 const getProjects = async (): Promise<Project[]> => {
+    console.log("GetProjects")
     const { data } = await clockify.get("/projects")
     return data
 }
@@ -36,7 +54,8 @@ const getProjects = async (): Promise<Project[]> => {
 
 const load = async () => {
     const projects = await getProjects()
-    const tasks = (await Promise.all(projects.map(p => getTasks(p)))).flat();
+    const tasks = await getAllTasks(projects);
+    console.log(tasks)
     console.log(`Tasks ${tasks.map(v => fullName(v))}`)
 }
 
